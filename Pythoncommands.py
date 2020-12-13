2976ef95c281c39ceb3a2730f92cd7e711f36196
NetAcad DEVASC <changeme@example.com>
2020-12-13 20:13:54 +0000

    Python scripts

---
+++ Pythoncommands.py
@@ -0,0 +1,106 @@
+#print("Welcome to DEVASC Course")
+
+#for alumnos in ["Adrian", "Jesus", "Edgar"]:
+#    print(alumnos)
+
+"""Esto es un ejemplo
+de
+lineas
+comentadas"""
+
+"""pip= 'cat'
+Age= 9
+Chill=True
+variable1=pip
+
+print(pip,variable1)"""
+
+#result=10/7
+#print(result)
+
+#result=10%7
+#print(result)
+
+#EjemploA=5<10
+#print(EjemploA)
+
+#EjemploB= 5=='5'
+#print(EjemploB)
+
+#String Values###
+#print (str.lower('HOOOLAAA'))
+#print (str.upper ('hola'))
+
+#Ejemplo1='hola'
+#print (str.capitalize(Ejemplo1))
+
+#########################Lists################
+#listavacia=[]
+#listavacia1=list()
+
+#for i in range (10):
+ #listavacia.append(i)
+ #print(listavacia)
+
+#Inges = ['Inge1','Inge2','Inge3','Inge4']
+#print(Inges)
+#list.clear(Inges)
+#list.reverse(Inges)
+#print(Inges)
+
+########INPUT##################
+#temperatura=float(input('Cual es la temperatura actual:'))
+#print(temperatura)
+
+#####Conditionals and loops#######
+
+#IF
+#n =int(input("Que edad tienes:"))
+#if n==20:
+# print (n)
+
+#Elif
+#n =int(input("Que edad tienes:"))
+#if n==20:
+# print (n)
+#elif n<=34:
+#    print('Estas Joven')
+#elif n>=35:
+#    print ('Ya tienes tus anios')
+
+#For
+#datos=(1,2,3,4,5)
+#for variable in datos:
+#    print(variable)
+
+#while True: 
+# string = input('Enter some text to print. \nType "done" to quit> ') 
+# if string == 'done': 
+#  break 
+# print (string) 
+#print('Done!')
+
+###################Classess and Functions########
+#def devnet():
+#    "Funcion para imprimir"
+#print ('hola mundo')
+
+#def hello(*args): 
+#    for arg in args: 
+#        print("Hello", arg, "!")
+#hello('Luis,Adrian,Jesus')
+
+#def hello(**kwargs): 
+#    for key, value in kwargs.items(): 
+#        print("Hello", value, "!")
+#hello(kwarg1='Adrian', kwarg2='Luis', kwarg3='Josh')
+
+#def greeting(name, message="Good morning!"): 
+#    print("Ola ke ase", name + ', ' + message)
+#greeting('Mau')
+#greeting('Mau','disfrutando de la sesion de python o ke ase')
+
+
+
+
+
---
+++ myCity.py
@@ -0,0 +1,6 @@
+def myCity(city):
+    print("I live in " + city + ".")
+
+myCity("CDMX")
+myCity("Chichuahua")
+myCity("Durango")
